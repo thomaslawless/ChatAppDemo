3644a48f89549bc760342d947ca2f712158157a5
Thomas Lawless <55810024+thomaslawless@users.noreply.github.com>
2022-07-26 10:45:38 -0400

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Thomas Lawless III on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
